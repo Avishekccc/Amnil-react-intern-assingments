3745d7935d5baf7d8efcb2492d8e34225f5081a3
abhishek shah <shahabhishek456@gmail.com>
2025-09-21 00:29:43 +0545

    assingment 3 add module

--- asingment-3-CRUD/script.js
+++ asingment-3-CRUD/script.js
@@ -1,7 +1,9 @@
+// import { deleteContact, editContact } from "./updatedelete.js";
+import validateInput from "./validateInput.js";
+
 const form = document.getElementById("contactForm");
 const tableBody = document.querySelector("table tbody");
-const addbtn = document.querySelector(".add-btn")
-
+const addbtn = document.querySelector(".add-btn");
 
 let contacts = JSON.parse(localStorage.getItem("contacts")) || [];
 let editIndex = null;
@@ -20,49 +22,55 @@ function renderDatatoTable() {
       <td>${c.email}</td>
       <td>${c.phone}</td>
       <td>
-        <button class="edit-btn" onclick="editContact(${i})">Edit</button>
-        <button class="delete-btn" onclick="deleteContact(${i})">Delete</button>
+        <button class="edit-btn">Edit</button>
+        <button class="delete-btn" >Delete</button>
       </td>
     `;
+
+    row
+      .querySelector(".edit-btn")
+      .addEventListener("click", () => editContact(i));
+    row
+      .querySelector(".delete-btn")
+      .addEventListener("click", () => deleteContact(i));
     tableBody.appendChild(row);
   });
 }
 
-// Validation
-function validateInput() {
-  let valid = true;
+// function validateInput() {
+//   let valid = true;
 
-  const username = form.username.value.trim();
-  const email = form.email.value.trim();
-  const phone = form.phone.value.trim();
+//   const username = form.username.value.trim();
+//   const email = form.email.value.trim();
+//   const phone = form.phone.value.trim();
 
-  const usernameError = document.getElementById("usernameError");
-  const emailError = document.getElementById("emailError");
-  const phoneError = document.getElementById("phoneError");
+//   const usernameError = document.getElementById("usernameError");
+//   const emailError = document.getElementById("emailError");
+//   const phoneError = document.getElementById("phoneError");
 
-  usernameError.textContent = "";
-  emailError.textContent = "";
-  phoneError.textContent = "";
+//   usernameError.textContent = "";
+//   emailError.textContent = "";
+//   phoneError.textContent = "";
 
-  if (username.length < 3) {
-    usernameError.textContent = "Username must be at least 3 characters.";
-    valid = false;
-  }
+//   if (username.length < 3) {
+//     usernameError.textContent = "Username must be at least 3 characters.";
+//     valid = false;
+//   }
 
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailPattern.test(email)) {
-    emailError.textContent = "Please enter a valid email.";
-    valid = false;
-  }
+//   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//   if (!emailPattern.test(email)) {
+//     emailError.textContent = "Please enter a valid email.";
+//     valid = false;
+//   }
 
-  const phonePattern = /^\d{10}$/;
-  if (!phonePattern.test(phone)) {
-    phoneError.textContent = "Phone must be 10 digits.";
-    valid = false;
-  }
+//   const phonePattern = /^\d{10}$/;
+//   if (!phonePattern.test(phone)) {
+//     phoneError.textContent = "Phone must be 10 digits.";
+//     valid = false;
+//   }
 
-  return valid;
-}
+//   return valid;
+// }
 
 // handel form summit
 form.addEventListener("submit", (e) => {
@@ -91,16 +99,16 @@ form.addEventListener("submit", (e) => {
   form.reset();
 });
 
-function editContact(i) {
+export function editContact(i) {
   const c = contacts[i];
   form.username.value = c.username;
   form.email.value = c.email;
   form.phone.value = c.phone;
   editIndex = i;
-  addbtn.innerText = "Update contact"
+  addbtn.innerText = "Update contact";
 }
 
-function deleteContact(i) {
+export function deleteContact(i) {
   contacts.splice(i, 1);
   save();
   renderDatatoTable();
---
+++ asingment-3-CRUD/validateInput.js
@@ -0,0 +1,38 @@
+// Validation
+const form = document.getElementById("contactForm");
+// const tableBody = document.querySelector("table tbody");
+
+export default function validateInput() {
+  let valid = true;
+
+  const username = form.username.value.trim();
+  const email = form.email.value.trim();
+  const phone = form.phone.value.trim();
+
+  const usernameError = document.getElementById("usernameError");
+  const emailError = document.getElementById("emailError");
+  const phoneError = document.getElementById("phoneError");
+
+  usernameError.textContent = "";
+  emailError.textContent = "";
+  phoneError.textContent = "";
+
+  if (username.length < 3) {
+    usernameError.textContent = "Username must be at least 3 characters.";
+    valid = false;
+  }
+
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(email)) {
+    emailError.textContent = "Please enter a valid email.";
+    valid = false;
+  }
+
+  const phonePattern = /^\d{10}$/;
+  if (!phonePattern.test(phone)) {
+    phoneError.textContent = "Phone must be 10 digits.";
+    valid = false;
+  }
+
+  return valid;
+}
